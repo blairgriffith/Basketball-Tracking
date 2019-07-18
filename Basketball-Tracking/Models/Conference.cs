8d01c9f96936dfe268869826df6f07ade984cb4c
Blair <bgriffit@oberlin.edu>
2019-07-18 11:50:36 -0400

    first commit

---
+++ Basketball-Tracking/Models/Conference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketballTracking.Models
+{
+    public class Conference
+    {
+        public int ConferenceID { get; set; }
+        public string Name { get; set; }
+        public virtual List<Team> Teams { get; set; }
+    }
+}
---
+++ Basketball-Tracking/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basketball-Tracking.Models
+{
+    public class Player
+{
+}
+}
