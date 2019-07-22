ffb01863ff7592c8830edc10be8f2dff0a26e372
Hakim Smith <hakim.smith19@yahoo.com>
2019-07-22 17:18:20 -0400

    working on frontend, added components, index, api-action, and app.js

---
+++ frontend/js/component/Player.js
@@ -0,0 +1,15 @@
+export default function Players(playerlist){
+    return `
+    <h1>Players</h1>
+    <ul>
+        ${playerlist.map(player => {
+        return `
+            <li>
+                <p>${player.playerId}</p>
+                <img src="${player.ImageURL}>
+            </li>
+            `
+        })}
+    
+    `
+}
